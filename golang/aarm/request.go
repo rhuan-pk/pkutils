904956794e59dbc9b72756fa951109aaeaa54d48
rhuan-pk <rhuan.pksf@gmail.com>
2022-12-28 15:53:35 -0300

    atualizado: golang/aarm/request.go: StatusCodeReturn(): corrigido para entender como "hit" qualquer código entre 200 e 299.

--- golang/aarm/request.go
+++ golang/aarm/request.go
@@ -7,7 +7,10 @@ import (
 
 // StatusCodeReturn é a função comum para código de status de requisição
 func StatusCodeReturn(endpoint string, response *http.ResponseWriter, httpStatusCode int) {
-	level := map[bool]string{true: "error", false: "hit"}[httpStatusCode != http.StatusOK]
+	level := map[bool]string{
+		true:  "error",
+		false: "hit",
+	}[(httpStatusCode < 200 || httpStatusCode > 299)]
 	responseMessage := http.StatusText(httpStatusCode)
 	http.Error(*response, responseMessage, httpStatusCode)
 	log.Println("endpoint \""+endpoint+"\" "+level+":", responseMessage+"!")
